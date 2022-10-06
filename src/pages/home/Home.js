0ca6fbb4108f62b6a96db079c7ce8f2c77b59471
Seth <Medeirosseth@gamil.com>
2022-10-06 13:27:52 -0700

    fetch files from firestore

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,5 @@
-import { useFetch } from "../../hooks/useFetch";
-
+import { projectFirestore } from "../../firebase/config";
+import { useEffect, useState } from "react";
 //styles
 import "./home.css";
 
@@ -7,7 +7,34 @@ import "./home.css";
 import ShowList from "../../components/ShowList";
 
 export default function Home() {
-  const { data, isPending, error } = useFetch("http://localhost:3000/shows");
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore
+      .collection("shows")
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          setError("no shows to load");
+          setIsPending(false);
+        } else {
+          let results = [];
+          snapshot.docs.forEach((doc) => {
+            results.push({ id: doc.id, ...doc.data() });
+          });
+          setData(results);
+          setIsPending(false);
+        }
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
+      });
+  }, []);
 
   return (
     <div className="home">
