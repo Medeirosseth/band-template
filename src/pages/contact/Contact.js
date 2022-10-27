8b7e0a0e23ef7013edf906a1f8df92a4ec0c1157
Seth <Medeirosseth@gamil.com>
2022-10-27 15:49:49 -0700

    implement email js

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,11 +1,33 @@
 import "./contact.css";
+import emailjs from "@emailjs/browser";
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useLogout } from "../../hooks/useLogout";
-import Login from "../../components/login/login";
+
 export default function Contact() {
   const { logout } = useLogout();
   const { user } = useAuthContext();
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_wfgh7tl",
+        "template_1oa90rn",
+        form.current,
+        "AygS3BMlgLwH79lv6"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
   return (
     <>
@@ -19,49 +41,54 @@ export default function Contact() {
           </Link>
         </div>
         <div className="contact-right">
-          <form>
-            <div className="contact-form">
-              <input
-                className="contact-input"
-                type="text"
-                placeholder="Email"
-                name="email"
-                required
-              />
-              <input
-                className="contact-input"
-                type="text"
-                placeholder="Name"
-                name="email"
-                required
-              />
-              <textarea
-                className="contact-area"
-                type="text"
-                placeholder="Name of group... date you are interested in...
-                  We will get back to you as soon as
-                  we can, but please be patient. We receive hundreds of booking
-                  request and we do our best to give each one the attention they
-                    deserve."
-                name="email"
-                required
-              />
-              <button>Submit</button>
-            </div>
-          </form>
-          <span className="booking">Booking: wfkcbooking@gmail.com</span>
-
-          {!user && <></>}
-          {user && (
-            <>
-              {user.displayName}
-              <button className="btn" onClick={logout}>
-                Logout
-              </button>
-            </>
-          )}
+          <button>Submit</button>
         </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+        <span className="booking">Booking: wfkcbooking@gmail.com</span>
+
+        {!user && <></>}
+        {user && (
+          <>
+            {user.displayName}
+            <button className="btn" onClick={logout}>
+              Logout
+            </button>
+          </>
+        )}
       </div>
     </>
   );
+  // <form>
+  //   <div className="contact-form">
+  //     <input
+  //       className="contact-input"
+  //       type="text"
+  //       placeholder="Email"
+  //       name="email"
+  //       required
+  //     />
+  //     <input
+  //       className="contact-input"
+  //       type="text"
+  //       placeholder="Name"
+  //       name="email"
+  //       required
+  //     />
+  //     <textarea
+  //       className="contact-area"
+  //       type="text"
+  //       placeholder="Name of group... date you are interested in...
+  //         We will get back to you as soon as
+  //         we can, but please be patient."
+  //       name="email"
+  //       required
+  //     />
 }
